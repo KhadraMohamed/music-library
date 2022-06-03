352456646adbe855bdfe092bfc5d10715e1e5fd6
khadra mohamed <khadramohamed@live.co.uk>
2022-06-03 19:57:11 +0100

    Our First Test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require ('express');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, respond) => {
-    respond.send('Hello World!')
-});
+app.use('/artist' , artistRouter);
+
 
 module.exports = app;
