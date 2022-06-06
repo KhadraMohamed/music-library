29fa32c118c816c12db471655325e820c27af938
khadra mohamed <khadramohamed@live.co.uk>
2022-06-06 16:57:09 +0100

    Deleting Artists

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 router.post('/', artistController.create);
 router.get('/', artistController.read);
-router.get('/:artistId', artistController.readById)
+router.get('/:artistId', artistController.readById);
 router.patch('/:artistId', artistController.update);
+router.delete('/:artistId' , artistController.delete);
 
 
 module.exports = router;
