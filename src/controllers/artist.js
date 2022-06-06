5fb8d48cfdccacd34d11994179272b2b626ed411
khadra mohamed <khadramohamed@live.co.uk>
2022-06-06 15:20:44 +0100

    Reading Artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,18 +1,33 @@
+const { router } = require('../app')
+const getDb = require('../services/db')
+
 exports.create = async (req, res) => {
-    const db = await getDb();
-    const { name, genre } = req.body;
+  const db = await getDb()
+  const { name, genre } = req.body
 
-try {
-    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)' , [
-        name,
-        genre,
-      ]);
+  try {
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+      name,
+      genre
+    ])
 
-res.sendStatus(201);
-} catch (err) {
-    res.sendStatus(500).json(err);
+    res.sendStatus(201)
+  } catch (err) {
+    res.sendStatus(500).json(err)
+  }
+  db.close()
 }
+ 
+exports.read = async (_, res) => {
+    const db = await getDb();
 
-db.close();
+    try {
+        const [artists] = await db.query('SELECT * FROM Artist');
 
+        res.status(200).json(artists);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  db.close();
 };
+
